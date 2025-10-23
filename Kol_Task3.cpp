858fc76f82fde1b09fbfca993d94cf0b634662f0
Timur <timurlu7@mail.ru>
2025-10-23 19:43:22 +0300

    Task 3

---
+++ Kol_Task3.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include "vector"
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+
+Node* reverse(Node* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+
+    Node* newHead = reverse(head->next);
+
+
+    head->next->next = head;  
+    head->next = nullptr;     
+
+    return newHead;  
+}
+
+void print(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        std::cout << current->data << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+Node* create(const std::vector<int>& values) {
+    if (values.empty()) return nullptr;
+
+    Node* head = new Node(values[0]);
+    Node* current = head;
+    for (size_t i = 1; i < values.size(); i++) {
+        current->next = new Node(values[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+std::vector<int> listToVector(Node* head) {
+    std::vector<int> result;
+    Node* current = head;
+    while (current != nullptr) {
+        result.push_back(current->data);
+        current = current->next;
+    }
+    return result;
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    try {
+        Node* head = create({ 1, 2, 3, 4, 5 });
+
+        std::cout << "Исходный список: ";
+        print(head);
+
+        head = reverse(head);
+
+        std::cout << "Развернутый список: ";
+        print(head);
+
+        return 0;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Произошла ошибка: " << e.what() << std::endl;
+        return 1;
+    }
+}
---
+++ UnitTest_Task3.cpp
@@ -0,0 +1,115 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Timur\proga\ОС\Kol_Task3\Kol_Task3\Kol_Task3.cpp"
+#include "vector"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestTask3
+{
+	TEST_CLASS(UnitTestTask3)
+	{
+	public:
+		
+        TEST_METHOD(TestReverseNormalList)
+        {
+            Node* head = create({ 1, 2, 3, 4, 5 });
+            std::vector<int> expected = { 5, 4, 3, 2, 1 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected);
+
+        }
+
+        TEST_METHOD(TestReverseSingleElement)
+        {
+            Node* head = create({ 42 });
+            std::vector<int> expected = { 42 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected);
+        }
+
+        TEST_METHOD(TestReverseEmptyList)
+        {
+            Node* head = nullptr;
+
+            Node* reversed = reverse(head);
+
+            Assert::IsNull(reversed);
+        }
+
+        TEST_METHOD(TestReverseTwoElements)
+        {
+            Node* head = create({ 1, 2 });
+            std::vector<int> expected = { 2, 1 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected);
+
+        }
+
+        TEST_METHOD(TestReverseThreeElements)
+        {
+            Node* head = create({ 1, 2, 3 });
+            std::vector<int> expected = { 3, 2, 1 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected);
+        }
+
+        TEST_METHOD(TestReverseListWithDuplicates)
+        {
+            Node* head = create({ 1, 2, 2, 3, 3, 3 });
+            std::vector<int> expected = { 3, 3, 3, 2, 2, 1 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected);
+        }
+
+        TEST_METHOD(TestReverseNegativeNumbers)
+        {
+            Node* head = create({ -5, -4, -3, -2, -1 });
+            std::vector<int> expected = { -1, -2, -3, -4, -5 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected, L"Список с отрицательными числами неправильно развернут");
+
+        }
+
+        TEST_METHOD(TestReverseMixedNumbers)
+        {
+            Node* head = create({ -2, -1, 0, 1, 2 });
+            std::vector<int> expected = { 2, 1, 0, -1, -2 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected, L"Список со смешанными числами неправильно развернут");
+        }
+
+        TEST_METHOD(TestReverseZeroElements)
+        {
+            Node* head = create({ 0, 0, 0 });
+            std::vector<int> expected = { 0, 0, 0 };
+
+            Node* reversed = reverse(head);
+            std::vector<int> actual = listToVector(reversed);
+
+            Assert::IsTrue(actual == expected, L"Список с нулями неправильно развернут");
+
+        }
+    };
+}
